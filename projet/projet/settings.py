5b2d0a96692fd83f3d75b16a1820d8943c830103
Fitiavana Harilalaina <harilalainafitiavana@gmail.com>
2026-01-15 21:14:17 +1000

    Modification database

--- projet/projet/settings.py
+++ projet/projet/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'projet.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.environ.get("DATABASE_PUBLIC_URL")
     )
 }
 
